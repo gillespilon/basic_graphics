0e2567747c15e0736dc27c3ee3e17e0609f611f2
Gilles Pilon <gillespilon13@gmail.com>
2020-01-15 12:16:48 -0600

    Add if __name__ ... and move code after this

--- line_plot_actual_vs_target.py
+++ line_plot_actual_vs_target.py
@@ -36,26 +36,24 @@ def despine(ax: axes.Axes) -> None:
         ax.spines[spine].set_visible(False)
 
 
-df = pd.read_csv('actual_vs_target.csv', parse_dates=['Date'])
-
-
-figure_width_height = (8, 6)
-axis_title = 'Plot of Savings Target vs Actual'
-x_axis_label = 'Date'
-y_axis_label = 'USD'
-fig = plt.figure(figsize=figure_width_height)
-ax = fig.add_subplot(111)
-ax.plot(df['Date'], df['Target'], label='Target', linestyle='-', color=c[0])
-ax.plot(df['Date'], df['Actual'], label='Actual', marker='o', linestyle='-',
-        color=c[1])
-ax.set_title(axis_title, fontweight='bold')
-ax.set_xlabel(x_axis_label, fontweight='bold')
-ax.set_ylabel(y_axis_label, fontweight='bold')
-ax.xaxis.set_major_locator(MonthLocator())
-ax.xaxis.set_minor_locator(NullLocator())
-ax.xaxis.set_major_formatter(DateFormatter('%y-%m'))
-ax.xaxis.set_minor_formatter(NullFormatter())
-ax.legend(frameon=False)
-despine(ax)
-ax.figure.savefig('target_vs_actual.svg', format='svg')
-ax.figure.savefig('target_vs_actual.png', format='png')
+if __name__ == '__main__':
+    df = pd.read_csv('actual_vs_target.csv', parse_dates=['Date'])
+    figure_width_height = (8, 6)
+    axis_title = 'Plot of Savings Target vs Actual'
+    x_axis_label = 'Date'
+    y_axis_label = 'USD'
+    fig = plt.figure(figsize=figure_width_height)
+    ax = fig.add_subplot(111)
+    ax.plot(df['Date'], df['Target'], label='Target',
+            linestyle='-', color=c[0])
+    ax.plot(df['Date'], df['Actual'], label='Actual', marker='o',
+            linestyle='-', color=c[1])
+    ax.set_title(axis_title, fontweight='bold')
+    ax.set_xlabel(x_axis_label, fontweight='bold')
+    ax.set_ylabel(y_axis_label, fontweight='bold')
+    ax.xaxis.set_major_locator(MonthLocator())
+    ax.xaxis.set_minor_locator(NullLocator())
+    ax.xaxis.set_major_formatter(DateFormatter('%y-%m'))
+    ax.xaxis.set_minor_formatter(NullFormatter())
+    ax.legend(frameon=False)
+    despine(ax)
