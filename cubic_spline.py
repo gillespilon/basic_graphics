28d768fd90ff78ef0b20ea253407bb1d5f4ecd5f
Gilles Pilon <gillespilon13@gmail.com>
2020-01-29 12:32:18 -0600

    Add def read_data_file
    - Replace x1 -> raw_data_x_values
    - Replace y1 -> raw_data_y_values
    - Rename def cubic_spline -> plot_cubic_spline

--- cubic_spline.py
+++ cubic_spline.py
@@ -19,6 +19,7 @@ import numpy as np
 import matplotlib.axes as axes
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+import pandas as pd
 from scipy.interpolate import CubicSpline as cs
 
 
@@ -31,13 +32,31 @@ def main():
         'Dependent value',
         'Cubic Spline'
     )
-    x1 = np.arange(10)  # create x values from 0 to 9, increments of 1
-    y1 = np.sin(x1)
+    raw_data = read_data_file()
+    raw_data_x_values = raw_data.iloc[:, 0]
+    raw_data_y_values = raw_data.iloc[:, 1]
     x2 = np.arange(-0.5, 9.6, 0.1)  # create x from 0.5 to 9.5, increment 0.1
-    y2 = cs(x1, y1)
-    ax = cubic_spline(x1, y1, x2, y2, x_axis_label, y_axis_label, axis_title)
+    y2 = cs(raw_data_x_values, raw_data_y_values)
+    ax = plot_cubic_spline(raw_data_x_values, raw_data_y_values,
+                      x2, y2,
+                      x_axis_label, y_axis_label, axis_title)
     despine(ax)
     ax.figure.savefig('cubic_spline.svg', format='svg')
+
+
+def read_data_file():
+    '''
+    The data file is presumed to have an index (row names or values),
+    a first column with x values, and a second column with y values.
+    '''
+    while True:
+        file_name = input('CSV file name to read? ')
+        try:
+            data = pd.read_csv(file_name, index_col=None)
+        except FileNotFoundError:
+            print(f'File {file_name} does not exist. Please try again.')
+        else:
+            return data
 
 
 def despine(ax: axes.Axes) -> None:
@@ -50,11 +69,13 @@ def despine(ax: axes.Axes) -> None:
         ax.spines[spine].set_visible(False)
 
 
-def cubic_spline(x1, y1, x2, y2, x_axis_label, y_axis_label, axis_title):
+def plot_cubic_spline(raw_data_x_values, raw_data_y_values,
+                 x2, y2,
+                 x_axis_label, y_axis_label, axis_title):
     figure_width_height = (8, 6)
     fig = plt.figure(figsize=figure_width_height)
     ax = fig.add_subplot(111)
-    ax.plot(x1, y1, marker='o', linestyle='None',
+    ax.plot(raw_data_x_values, raw_data_y_values, marker='o', linestyle='None',
             color=c[1], label='data')
     ax.plot(x2, y2(x2), marker='None', linestyle='-',
             color=c[5], label='cubic spline')
