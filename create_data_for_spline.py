ae91421541ae0d0a7a2b635b3b9ece123e72d57e
Gilles Pilon <gillespilon13@gmail.com>
2020-02-05 12:15:26 -0600

    Create two functions and if __name ...
    - Create def main()
    - Create def create_data()
    -Create if __name__ == '__main__':

--- create_data_for_spline.py
+++ create_data_for_spline.py
@@ -12,13 +12,23 @@ import pandas as pd
 import numpy as np
 
 
-filename = ('cubic_spline_raw_data.csv')
-columns = ['raw_data_x', 'raw_data_y']
-raw_data_x_values = np.arange(10)
-raw_data_y_values = np.sin(raw_data_x_values)
-array = np.array([raw_data_x_values, raw_data_y_values])
-data = pd.DataFrame(
-    array.T,
-    columns=columns
-)
-data.to_csv(filename, index=False)
+def main():
+    filename = ('cubic_spline_raw_data.csv')
+    columns = ['raw_data_x', 'raw_data_y']
+    data = create_data(filename, columns)
+    data.to_csv(filename, index=False)
+
+
+def create_data(filename, columns):
+    raw_data_x_values = np.arange(10)
+    raw_data_y_values = np.sin(raw_data_x_values)
+    array = np.array([raw_data_x_values, raw_data_y_values])
+    data = pd.DataFrame(
+        array.T,
+        columns=columns
+    )
+    return data
+
+
+if __name__ == '__main__':
+    main()
