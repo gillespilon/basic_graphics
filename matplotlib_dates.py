ecd85e636ff1889246cbaef3c3f947f71b1f45bc
Gilles Pilon <gillespilon13@gmail.com>
2020-07-24 11:17:25 -0500

    Test smoothing for line, scatter plots

--- matplotlib_dates.py
+++ matplotlib_dates.py
@@ -33,29 +33,32 @@ def main():
     c = cm.Paired.colors
     pd.set_option('display.max_columns', 600)
     pd.set_option('display.max_rows', 600)
-    file_name_graph = 'matplotlib_dates_line_plot.svg'
     figure_width_height = (8, 6)
     figure_title = 'Figure title'
     axis_title = 'Axis title'
     abscissa_label = 'abscissa'
     ordinate_label = 'ordinate'
-    column_abscissa_one = 'dates_one'
-    column_abscissa_two = 'dates_two'
+    column_abscissa_datefloat_one = 'datefloats_one'
+    column_abscissa_datetime_one = 'datetimes_one'
+    column_abscissa_datefloat_two = 'datefloats_two'
+    column_abscissa_datetime_two = 'datetimes_two'
+    column_abscissa_one = 'abscissa_one'
+    column_abscissa_two = 'abscissa_two'
     column_ordinate_one = 'ordinate_one'
     column_ordinate_two = 'ordinate_two'
     raw = {
-        'abscissa_one': [
+        column_abscissa_one: [
             '2018-07-31', '2018-08-04', '2018-08-06', '2018-08-11',
             '2018-08-12', '2018-08-15', '2018-08-16', '2018-08-17',
             '2018-08-18', '2018-08-25', '2018-09-15'
         ],
-        'abscissa_two': [
+        column_abscissa_two: [
             '2019-07-11', '2019-07-14', '2019-07-16', '2019-07-21',
             '2019-08-12', '2019-08-13', '2019-08-16', '2019-08-18',
             '2019-08-21', '2019-08-25', '2019-09-05'
         ],
-        'ordinate_one': [10, 15, 30, 35, 40, 45, 40, 30, 35, 50, 75],
-        'ordinate_two': [20, 35, 20, 15, 30, 45, 50, 40, 45, 50, 65]
+        column_ordinate_one: [10, 15, 30, 35, 40, 45, 40, 30, 35, 50, 75],
+        column_ordinate_two: [20, 35, 20, 15, 30, 45, 50, 40, 45, 50, 65]
     }
     data = pd.DataFrame(data=raw)
     print(
@@ -63,26 +66,30 @@ def main():
         f'{data}\n'
     )
     # Creates a list of datetime objects
-    data['datetimes_one'] = str_to_datetime(data['abscissa_one'])
-    data['dates_one'] = [mdates.date2num(date)
-                         for date in data['datetimes_one']]
-    data['datetimes_two'] = str_to_datetime(data['abscissa_two'])
-    data['dates_two'] = [mdates.date2num(date)
-                         for date in data['datetimes_two']]
+    data[column_abscissa_datetime_one] =\
+        str_to_datetime(data[column_abscissa_one])
+    data[column_abscissa_datefloat_one] =\
+        [mdates.date2num(date)
+         for date in data[column_abscissa_datetime_one]]
+    data[column_abscissa_datetime_two] =\
+        str_to_datetime(data[column_abscissa_two])
+    data[column_abscissa_datefloat_two] =\
+        [mdates.date2num(date)
+         for date in data[column_abscissa_datetime_two]]
     print(
         f'DataFrame:\n'
         f'{data}\n'
     )
     print(
         f'Column data types\n'
-        f'abscissa_one  : {data["abscissa_one"].dtype}\n'
-        f'abscissa_two  : {data["abscissa_two"].dtype}\n'
-        f'ordinate_one  : {data["ordinate_one"].dtype}\n'
-        f'ordinate_two  : {data["ordinate_two"].dtype}\n'
-        f'datetimes_one : {data["datetimes_one"].dtype}\n'
-        f'dates_one     : {data["dates_one"].dtype}\n'
-        f'datetimes_two : {data["datetimes_two"].dtype}\n'
-        f'dates_two     : {data["dates_two"].dtype}\n'
+        f'abscissa_one  : {data[column_abscissa_one].dtype}\n'
+        f'abscissa_two  : {data[column_abscissa_two].dtype}\n'
+        f'ordinate_one  : {data[column_ordinate_one].dtype}\n'
+        f'ordinate_two  : {data[column_ordinate_two].dtype}\n'
+        f'datetimes_one : {data[column_abscissa_datetime_one].dtype}\n'
+        f'dates_one     : {data[column_abscissa_datefloat_one].dtype}\n'
+        f'datetimes_two : {data[column_abscissa_datetime_two].dtype}\n'
+        f'dates_two     : {data[column_abscissa_datefloat_two].dtype}\n'
     )
     # plot_line_two_subplots(
     #     data, column_abscissa, column_ordinate_one,
@@ -91,48 +98,78 @@ def main():
     # )
     # Test line plot x y, smoothing None
     fig, ax = ds.plot_line_x_y(
-        data['datetimes_one'],
-        data['ordinate_one'],
-        figure_width_height
+        data[column_abscissa_datetime_one],
+        data[column_ordinate_one]
     )
     plot_pretty(
         fig,
         ax,
         'matplotlib_dates_line_plot.svg',
-        figure_title, axis_title,
         abscissa_label,
         ordinate_label,
         c
     )
     # Test scatter plot x y, smoothing None
     fig, ax = ds.plot_scatter_x_y(
-        data['datetimes_one'],
-        data['ordinate_one'],
+        data[column_abscissa_datetime_one],
+        data[column_ordinate_one],
         figure_width_height
     )
     plot_pretty(
         fig,
         ax,
         'matplotlib_dates_scatter_plot.svg',
-        figure_title,
-        axis_title,
         abscissa_label,
         ordinate_label,
-        c
+        c,
+        figure_title,
+        axis_title,
     )
     # Test line plot x y, smoothing = 'natural_cubic_spline'
+    fig, ax = ds.plot_line_x_y(
+        data[column_abscissa_datetime_one],
+        data[column_ordinate_one],
+        smoothing='natural_cubic_spline',
+        numknots=5
+    )
+    plot_pretty(
+        fig,
+        ax,
+        'matplotlib_dates_line_plot_smoothing.svg',
+        abscissa_label,
+        ordinate_label,
+        c,
+        axistitle=axis_title,
+        figuretitle=None,
+    )
     # Test scatter plot x y, smoothing = 'natural_cubic_spline'
+    fig, ax = ds.plot_scatter_x_y(
+        data[column_abscissa_datetime_one],
+        data[column_ordinate_one],
+        figure_width_height,
+        smoothing='natural_cubic_spline',
+        numknots=5
+    )
+    plot_pretty(
+        fig,
+        ax,
+        'matplotlib_dates_scatter_plot_smoothing.svg',
+        abscissa_label,
+        ordinate_label,
+        c,
+        figure_title,
+    )
 
 
 def plot_pretty(
     fig: plt.figure,
     ax: axes.Axes,
-    filenamegraph: str,
-    figuretitle: str,
-    axistitle: str,
-    abscissalabel: str,
-    ordinatelabel: str,
-    c: Tuple[Tuple[float]]
+    filenamegraph: str = None,
+    abscissalabel: str = None,
+    ordinatelabel: str = None,
+    c: Tuple[Tuple[float]] = None,
+    figuretitle: str = None,
+    axistitle: str = None
 ) -> None:
     despine(ax)
     fig.suptitle(figuretitle, fontweight='bold', fontsize=16)
