11d6d91c810f9e1f26b23434a5bb3a7c5f64b35c
Gilles Pilon <gillespilon13@gmail.com>
2020-09-02 11:14:56 -0500

    Replace code with call to datasense, add to docstrings

--- weight_loss_tracker.py
+++ weight_loss_tracker.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 '''
-Create a line plot of weight and a fixed target line
+Create line plots of actual and target for tracking weight loss.
 
 time -f '%e' ./weight_loss_tracker.py
 ./weight_loss_tracker.py
@@ -11,9 +11,6 @@ time -f '%e' ./weight_loss_tracker.py
 # - Confidence interval
 # - Prediction interval
 
-from typing import Tuple
-
-import matplotlib.pyplot as plt
 import matplotlib.axes as axes
 import matplotlib.cm as cm
 import datasense as ds
@@ -22,32 +19,38 @@ import pandas as pd
 
 c = cm.Paired.colors
 figure_width_height = (8, 6)
-fig_title = 'Weight loss'
-file_name_data = 'weight.ods'
-file_name_graph = 'weight.svg'
-column_x = 'Date'
-column_target = 'Target'
-column_actual = 'Actual'
+file_name_data, file_name_graph = (
+    'weight.ods',
+    'weight.svg'
+)
+column_x, column_target, column_actual = (
+    'Date',
+    'Target',
+    'Actual'
+)
 x_axis_label, y_axis_label, axis_title = (
     'Date',
     'Weight (kg)',
-    'Plot of Weight Loss'
+    'Weight Loss'
 )
 
 
 def main():
     data = read_data(file_name_data)
-    plot_line(
-        dataframe=data,
-        columnx=column_x,
-        columntarget=column_target,
-        columnactual=column_actual,
-        filenamegraph=file_name_graph,
-        figurewidthheight=figure_width_height,
-        title=axis_title,
-        xlabel=x_axis_label,
-        ylabel=y_axis_label
+    fig, ax = ds.plot_line_line_x_y1_y2(
+              X=data[column_x],
+              y1=data[column_target],
+              y2=data[column_actual],
+              figuresize=figure_width_height,
+              labellegendy1=column_target,
+              labellegendy2=column_actual
     )
+    ax.set_title(label=axis_title, fontweight='bold')
+    ax.set_xlabel(x_axis_label, fontweight='bold')
+    ax.set_ylabel(y_axis_label, fontweight='bold')
+    ax.legend(frameon=False)
+    despine(ax)
+    ax.figure.savefig(file_name_graph, format='svg')
 
 
 def despine(ax: axes.Axes) -> None:
@@ -61,47 +64,21 @@ def despine(ax: axes.Axes) -> None:
 
 
 def read_data(filename: str) -> pd.DataFrame:
+    '''
+    Read the data into a dataframe.
+
+    Parameters:
+        filename    : str
+
+    Returns:
+        data        : pd.DataFrame
+    '''
     data = pd.read_excel(
         filename,
         engine='odf',
         parse_dates=['Date']
     )
     return data
-
-
-def plot_line(
-    dataframe: pd.DataFrame,
-    columnx: str,
-    columntarget: str,
-    columnactual: str,
-    filenamegraph: str,
-    figurewidthheight: Tuple[int, int],
-    title: str,
-    xlabel: str,
-    ylabel: str
-) -> None:
-    fig = plt.figure(figsize=figurewidthheight)
-    ax = fig.add_subplot(111)
-    ax.plot(
-        dataframe[columnx],
-        dataframe[columntarget],
-        color=c[0]
-    )
-    ax.plot(
-        dataframe[columnx],
-        dataframe[columnactual],
-        color=c[1],
-        marker='.',
-        markersize=10,
-        linestyle='-'
-    )
-    ax.set_title(fig_title, fontweight='bold')
-    ax.set_xlabel(xlabel, fontweight='bold')
-    ax.set_ylabel(ylabel, fontweight='bold')
-    ds.format_dates(fig, ax)
-    despine(ax)
-    fig.autofmt_xdate()
-    ax.figure.savefig(filenamegraph, format='svg')
 
 
 if __name__ == '__main__':
