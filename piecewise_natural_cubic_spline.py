65a7ec72f6dfe61973ab186f8d40b5eafd2667e6
Gilles Pilon <gillespilon13@gmail.com>
2020-10-14 11:48:07 -0500

    Update read_file, html output

--- piecewise_natural_cubic_spline.py
+++ piecewise_natural_cubic_spline.py
@@ -43,10 +43,10 @@ import ezgmail
 
 def main():
     start_time = time.time()
-    global figure_width_height, axis_title, x_axis_label, y_axis_label,\
+    global figsize, axis_title, x_axis_label, y_axis_label,\
         graphics_directory
     file_names, targets, features, number_knots, graphics_directory, \
-        figure_width_height, x_axis_label, y_axis_label, axis_title, \
+        figsize, x_axis_label, y_axis_label, axis_title, \
         date_time_parser, output_url, header_title, header_id = parameters()
     set_up_graphics_directory(graphics_directory)
     original_stdout = ds.html_begin(
@@ -54,18 +54,18 @@ def main():
         headertitle=header_title,
         headerid=header_id
     )
-    for file, target, feature in itertools.product(
-        file_names, targets, features
-    ):
+    ds.page_break()
+    print('<pre style="white-space: pre-wrap;">')
+    for file_name, target, feature in zip(file_names, targets, features):
         data = ds.read_file(
-            filename=file,
-            abscissa=feature
+            file_name=file_name,
+            parse_dates=features
         )
         data[target] = data[target].fillna(data[target].mean())
         dates = True
         X = pd.to_numeric(data[feature])
         y = data[target]
-        t = ((X, y, file, target, feature, knot, dates)
+        t = ((X, y, file_name, target, feature, knot, dates)
              for knot in number_knots)
         with Pool() as pool:
             for _ in pool.imap_unordered(plot_scatter_line, t):
@@ -73,7 +73,7 @@ def main():
         for knot in number_knots:
             print(
                 f'<p><img src="{graphics_directory}/'
-                f'spline_{file.strip(".csv")}_'
+                f'spline_{file_name.strip(".csv")}_'
                 f'{target}_{feature}_{knot}.svg"/></p>'
             )
     page_break()
@@ -81,11 +81,12 @@ def main():
     elapsed_time = stop_time - start_time
     summary(
         elapsedtime=elapsed_time,
-        filenames=file_names,
+        file_names=file_names,
         targets=targets,
         features=features,
         number_knots=number_knots
     )
+    print('</pre>')
     ds.html_end(
         originalstdout=original_stdout,
         outputurl=output_url
@@ -118,16 +119,16 @@ def parameters(
     '''
 
     parameters = ds.read_file(
-        filename='piecewise_natural_cubic_spline_parameters.ods'
+        file_name='piecewise_natural_cubic_spline_parameters.ods'
     )
-    filenames = [x for x in parameters['File names'] if str(x) != 'nan']
+    file_names = [x for x in parameters['File names'] if str(x) != 'nan']
     targets = [x for x in parameters['Targets'] if str(x) != 'nan']
     features = [x for x in parameters['Features'] if str(x) != 'nan']
     number_knots = [int(x) for x in parameters['Number of knots']
-                if str(x) != 'nan']
+                    if str(x) != 'nan']
     datetimeparser = parameters['Other parameter values'][0]
     graphicsdirectory = parameters['Other parameter values'][1]
-    figurewidthheight = eval(parameters['Other parameter values'][2])
+    figsize = eval(parameters['Other parameter values'][2])
     xaxislabel = parameters['Other parameter values'][3]
     yaxislabel = parameters['Other parameter values'][4]
     axistitle = parameters['Other parameter values'][5]
@@ -135,8 +136,8 @@ def parameters(
     headertitle = parameters['Other parameter values'][7]
     headerid = parameters['Other parameter values'][8]
     return (
-        filenames, targets, features, number_knots, graphicsdirectory,
-        figurewidthheight, xaxislabel, yaxislabel, axistitle,
+        file_names, targets, features, number_knots, graphicsdirectory,
+        figsize, xaxislabel, yaxislabel, axistitle,
         datetimeparser, outputurl, headertitle, headerid
     )
 
@@ -152,7 +153,7 @@ def page_break() -> None:
 
 def summary(
     elapsedtime: float,
-    filenames: List[str],
+    file_names: List[str],
     targets: List[str],
     features: List[str],
     number_knots: List[int]
@@ -163,7 +164,7 @@ def summary(
 
     print('<h1>Report summary</h1>')
     print(f'Execution time : {elapsedtime:.3f} s')
-    print(f'Files read     : {filenames}')
+    print(f'Files read     : {file_names}')
     print(f'Targets        : {targets}')
     print(f'Features       : {features}')
     print(f'Number of knots: {number_knots}')
@@ -197,7 +198,7 @@ def plot_scatter_line(
         X=XX,
         y1=y,
         y2=model.predict(X),
-        figuresize=figure_width_height,
+        figsize=figsize,
         labellegendy2=f'number knots = {number_knots}'
     )
     ax.legend(frameon=False, loc='best')
@@ -208,7 +209,7 @@ def plot_scatter_line(
     )
     ax.set_xlabel(x_axis_label)
     ax.set_ylabel(y_axis_label)
-    despine(ax)
+    ds.despine(ax)
     ax.figure.savefig(
         f'{graphics_directory}'
         f'/spline_'
@@ -217,22 +218,6 @@ def plot_scatter_line(
         f'{number_knots}.svg',
         format='svg'
     )
-
-
-def despine(ax: axes.Axes) -> None:
-    """
-    Remove the top and right spines of a graph.
-
-    Parameters
-    ----------
-    ax : axes.Axes
-
-    Example
-    -------
-    >>> despine(ax)
-    """
-    for spine in 'right', 'top':
-        ax.spines[spine].set_visible(False)
 
 
 if __name__ == '__main__':
