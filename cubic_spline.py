51a80f0b23fa140b5cbe9bb10d09b4f378d57633
Gilles Pilon <gillespilon13@gmail.com>
2020-10-16 20:32:20 -0500

    Fix savefig

--- cubic_spline.py
+++ cubic_spline.py
@@ -182,7 +182,7 @@ def plot_graph(
     ax.set_xlabel(xaxislabel, fontweight='bold')
     ax.set_ylabel(yaxislabel, fontweight='bold')
     despine(ax)
-    ax.figure.savefig(f'{graphname}.svg', format='svg')
+    fig.savefig(f'{graphname}.svg', format='svg')
 
 
 if __name__ == '__main__':
