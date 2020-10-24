5554cc5a5bb3c0c3ea9a8d57fa190addb8bdc196
Gilles Pilon <gillespilon13@gmail.com>
2020-10-24 18:26:43 -0500

    Fix datetimeparser

--- exponentially_weighted_average.py
+++ exponentially_weighted_average.py
@@ -41,7 +41,7 @@ def main():
         abscissa_name,
         ordinate_name,
         ordinate_predicted_name,
-        datetimeparser,
+        date_time_parser,
         columnnamessort,
         dateformatter,
         graphfilename
@@ -55,7 +55,7 @@ def main():
         date_formatter,
         graph_file_names
     ):
-        if datetimeparser == 'None':
+        if date_time_parser == 'None':
             data = ds.read_file(
                 file_name=file_name,
                 # parse_dates=abscissa_name,
@@ -145,7 +145,7 @@ def parameters() -> (
     figurewidthheight = eval(parameters['Other parameter values'][3])
     columnnamessort = [x for x in parameters['Column names sort']
                        if str(x) != 'nan']
-    datetimeparser = [x for x in parameters['Date time parser']
+    date_time_parser = [x for x in parameters['Date time parser']
                       if str(x) != 'nan']
     parser = parameters['Other parameter values'][4]
     dateformatter = [None
@@ -164,7 +164,7 @@ def parameters() -> (
     return (
         filenames, graphfilenames, abscissa_names, ordinate_names,
         ordinate_predicted_names, xaxislabel, yaxislabel, axistitle,
-        figurewidthheight, columnnamessort, datetimeparser, dateformatter,
+        figurewidthheight, columnnamessort, date_time_parser, dateformatter,
         alphavalue, function, output_url, header_title, header_id, parser
     )
 
