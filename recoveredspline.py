01188e4edb0f816b6a00d6d5c0ded1ea019b8747
Gilles Pilon <gillespilon13@gmail.com>
2020-11-06 15:35:42 -0600

    Fix xlabel

--- recoveredspline.py
+++ recoveredspline.py
@@ -44,7 +44,10 @@ def cubic_spline(x1, y1, x2, y2, x_axis_label, y_axis_label, axis_title):
     ax.plot(x2, y2(x2), marker='None', linestyle='-',
             color=colour2, label='cubic spline')
     ax.set_title(axis_title, fontweight='bold')
-    ax.set_xlabel(x_axis_label, fontweight='bold')
+    ax.set_xlabel(
+        xlabel=x_axis_label,
+        fontweight='bold'
+    )
     ax.set_ylabel(y_axis_label, fontweight='bold')
     ax.legend(frameon=False)
     return ax
