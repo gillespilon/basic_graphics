4c5654b051f81f1d3e574b404477b927932bda9e
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-10-26 09:46:14 -0500

    Refactor code to simplify statsmodels multiple regression

--- statsmodels_multiple_linear_regression.py
+++ statsmodels_multiple_linear_regression.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 """
-Demonstrate multiple linear regression with statsmodels.
+Demonstrate multiple linear regression, confidence interval, and prediction
+interval with statsmodels
 """
 
 import matplotlib.pyplot as plt
@@ -10,99 +11,107 @@ import pandas as pd
 
 
 def main():
-    series_y = ds.random_data()
-    series_x1 = ds.random_data()
-    series_x2 = ds.random_data()
+    output_url = "statsmodels_multiple_linear_regression.html"
+    header_title = "Statsmodels multiple linear regression"
+    header_id = "statsmodels-multiple-linear-regression"
+    original_stdout = ds.html_begin(
+        output_url=output_url,
+        header_title=header_title,
+        header_id=header_id
+    )
+    series_year = [
+        2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017,
+        2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016, 2016
+    ]
+    series_month = [
+        12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3,
+        2, 1
+    ]
+    series_interest_rate = [
+        2.75, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.25, 2.25, 2.25, 2, 2, 2, 1.75,
+        1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75
+    ]
+    series_unemployment_rate = [
+        5.3, 5.3, 5.3, 5.3, 5.4, 5.6, 5.5, 5.5, 5.5, 5.6, 5.7, 5.9, 6, 5.9,
+        5.8, 6.1, 6.2, 6.1, 6.1, 6.1, 5.9, 6.2, 6.2, 6.1
+    ]
+    series_index_price = [
+        1464, 1394, 1357, 1293, 1256, 1254, 1234, 1195, 1159, 1167, 1130, 1075,
+        1047, 965, 943, 958, 971, 949, 884, 866, 876, 822, 704, 719
+    ]
+    x_column = ["interest_rate", "unemployment_rate"]
+    prediction_column = "mean"
+    y_column = "index_price"
+    figsize = (8, 6)
+    labellegendy1 = "Data"
+    labellegendy2 = "Linear regression"
+    graphnames = [
+        "interest_rate_vs_index_price.svg",
+        "unemployment_rate_vs_index_price.svg"
+    ]
     df = pd.DataFrame(
-        {
-            'x1': series_x1,
-            'x2': series_x2,
-            'y': series_y
+        data={
+            "year": series_year,
+            "month": series_month,
+            x_column[0]: series_interest_rate,
+            x_column[1]: series_unemployment_rate,
+            y_column: series_index_price
         }
     )
-    x = sm.add_constant(df[['x1', 'x2']])
-    y = df['y']
-    model = sm.OLS(
-        endog=y,
-        exog=x,
-        missing='drop'
+    df_predictions = ds.linear_regression(
+        df=df,
+        x_column=x_column,
+        y_column=y_column,
+        prediction_column=prediction_column
     )
-    results = model.fit()
-    df['y_predicted'] = results.predict(x)
-    results.summary()
-    df = df.sort_values(by=['x1'])
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(111)
-    ax.plot(
-        df['x1'],
-        df['y'],
-        marker='.',
-        linestyle='None',
-        color='#0077bb'
-    )
-    ax.plot(
-        df['x1'],
-        df['y_predicted'],
-        marker='.',
-        linestyle='None',
-        color='#cc3311'
-    )
-    ax.set_title(
-        label='Regression analysis',
-        fontsize=15,
-        
-    )
-    ax.set_xlabel(
-        xlabel='X axis label',
-        fontsize=12,
-        
-    )
-    ax.set_ylabel(
-        ylabel='Y axis label',
-        fontsize=12,
-        
-    )
-    fig.savefig(
-        fname='y_vs_x1.svg',
-        format='svg'
-    )
-    df = df.sort_values(by=['x2'])
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(111)
-    ax.plot(
-        df['x2'],
-        df['y'],
-        marker='.',
-        linestyle='None',
-        color='#0077bb'
-    )
-    ax.plot(
-        df['x2'],
-        df['y_predicted'],
-        marker='.',
-        linestyle='None',
-        color='#cc3311'
-    )
-    ax.set_title(
-        label='Regression analysis',
-        fontsize=15,
-        
-    )
-    ax.set_xlabel(
-        xlabel='X axis label',
-        fontsize=12,
-        
-    )
-    ax.set_ylabel(
-        ylabel='Y axis label',
-        fontsize=12,
-        
-    )
-    fig.savefig(
-        fname='y_vs_x2.svg',
-        format='svg'
+    ds.style_graph()
+    # need to figure out how to plot multiple regression graphs
+    # for X, graphname in zip(x_column, graphnames):
+    #     print(df_predictions[X])
+    #     fig, ax = ds.plot_scatter_line_x_y1_y2(
+    #         X=df_predictions[X],
+    #         y1=df_predictions[y_column],
+    #         y2=df_predictions[prediction_column],
+    #         figsize=figsize,
+    #         labellegendy1=labellegendy1,
+    #         labellegendy2=labellegendy2
+    #     )
+    # ax.plot(
+    #     df["interest_rate"],
+    #     df[y_column],
+    #     marker=".",
+    #     linestyle="None",
+    #     color="#0077bb"
+    # )
+    # ax.plot(
+    #     df["interest_rate"],
+    #     df[y_column],
+    #     marker=".",
+    #     linestyle="None",
+    #     color="#cc3311"
+    # )
+    #     ax.set_title(
+    #         label="Regression analysis",
+    #         fontsize=15
+    #         )
+    #     ax.set_xlabel(
+    #         xlabel="X axis label",
+    #         fontsize=12
+    #     )
+    #     ax.set_ylabel(
+    #         ylabel="Y axis label",
+    #         fontsize=12
+    #     )
+    #     fig.savefig(
+    #         fname=graphname,
+    #         format="svg"
+    #     )
+    #     ds.html_figure(file_name=graphname)
+    ds.html_end(
+        original_stdout=original_stdout,
+        output_url=output_url
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
