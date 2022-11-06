ca21388935f1a6598eb5bf187f9ce98f9bc2d632
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-06 11:13:34 -0600

    Use plt.subplots

--- box_plot.py
+++ box_plot.py
@@ -13,18 +13,21 @@ import pandas as pd
 import numpy as np
 
 
-figure_size = (12, 8)
-np.random.seed(10)
 nparray1 = np.random.normal(100, 10, 200)
 nparray2 = np.random.normal(80, 30, 200)
 nparray3 = np.random.normal(90, 20, 200)
 nparray4 = np.random.normal(70, 25, 200)
+np.random.seed(10)
+figsize = (12, 8)
 # use np.ndarray
 data = [nparray1, nparray2, nparray3, nparray4]
 print(type(nparray1))
 print(type(data))
-fig = plt.figure(figsize=figure_size)
-ax = fig.add_subplot(111)
+fig, ax = plt.subplots(
+    nrows=1,
+    ncols=1,
+    figsize=figsize
+)
 ax.boxplot(
         data,
         notch=True
@@ -37,8 +40,11 @@ data2 = pd.DataFrame(data=[nparray1, nparray2, nparray3, nparray4])\
         labels=['a', 'b', 'c', 'd'],
         axis='columns'
     )
-fig = plt.figure(figsize=figure_size)
-ax = fig.add_subplot(111)
+fig, ax = plt.subplots(
+    nrows=1,
+    ncols=1,
+    figsize=figsize
+)
 ax.boxplot(
         [data2['a'], data2['b'], data2['c'], data2['d']],
         notch=True
