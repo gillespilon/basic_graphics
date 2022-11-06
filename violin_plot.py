074ad6b718ca7ada30899ecffe289cc980e03b4c
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-11-06 11:02:06 -0600

    Use plt.subplots

--- violin_plot.py
+++ violin_plot.py
@@ -13,7 +13,7 @@ import pandas as pd
 import numpy as np
 
 
-figure_size = (12, 8)
+figsize = (12, 8)
 np.random.seed(10)
 nparray1 = np.random.normal(100, 10, 200)
 nparray2 = np.random.normal(80, 30, 200)
@@ -23,8 +23,11 @@ nparray4 = np.random.normal(70, 25, 200)
 data = [nparray1, nparray2, nparray3, nparray4]
 print(type(nparray1))
 print(type(data))
-fig = plt.figure(figsize=figure_size)
-ax = fig.add_subplot(111)
+fig, ax = plt.subplots(
+    nrows=1,
+    ncols=1,
+    figsize=figsize
+)
 ax.violinplot(
         data,
         showmedians=True
@@ -39,8 +42,11 @@ data2 = pd.DataFrame(data=[nparray1, nparray2, nparray3, nparray4])\
         labels=['a', 'b', 'c', 'd'],
         axis='columns'
     )
-fig = plt.figure(figsize=figure_size)
-ax = fig.add_subplot(111)
+fig, ax = plt.subplots(
+    nrows=1,
+    ncols=1,
+    figsize=figsize
+)
 ax.violinplot(
         [data2['a'], data2['b'], data2['c'], data2['d']],
         showmedians=True
