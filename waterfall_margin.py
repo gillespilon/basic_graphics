287574888f489473d873486208f79238e730bded
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-01-29 14:52:50 -0600

    Add figure size, remove pandas

--- waterfall_margin.py
+++ waterfall_margin.py
@@ -6,12 +6,14 @@ positive or negative values. These values can be time-based or category-based.
 Based on code by Chris Moffitt:
 https://pbpython.com/waterfall-chart.html
 
-TODO: add code to read an Excel or csv file.
+See also:
+https://matplotlib.org/stable/tutorials/artists.html
+https://matplotlib.org/stable/api/_as_gen/matplotlib.artist.setp.html
 """
 
 import matplotlib.pyplot as plt
+import matplotlib.artist as mpla
 import datasense as ds
-import pandas as pd
 import numpy as np
 
 
@@ -27,6 +29,7 @@ def main():
     GRID_ALPHA = 0.2
     YLIM_MIN = 15000
     YLIM_MAX = 40000
+    FIGSIZE = (8, 6)
     FORMAT = "svg"
     df = ds.read_file(file_name=FILE_NAME)
     xlabel = df.columns[0]
@@ -41,7 +44,7 @@ def main():
     step = df_blank.reset_index(drop=True).repeat(3).shift(-1)
     step[1::3] = np.nan
     df_blank.loc[LAST_COLUMN] = 0
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=FIGSIZE)
     x = df.index  # bar positions
     # create the waterfall chart, no need for a stacked argument
     ax.bar(x=x, height=df[amount], width=0.4, bottom=df_blank)
@@ -57,13 +60,19 @@ def main():
     ax.patches[0].set_facecolor(FIRST_BAR_COLOUR)
     ax.patches[-1].set_facecolor(LAST_BAR_COLOUR)
     ax.plot(step.index, step.values, "b", linewidth=0.5)
-    ax.tick_params(axis="x", labelsize=14, labelrotation=90)
+    ax.tick_params(axis="x", labelsize=14,)
     ax.tick_params(axis="y", labelsize=14)
     ax.grid(visible=True, which="major", axis="y", alpha=GRID_ALPHA)
     ax.set_ylabel(ylabel=ylabel, weight="bold", fontsize=16)
     ax.set_xlabel(xlabel=xlabel, weight="bold", fontsize=16)
     ax.set_title(label=TITLE, weight="bold", fontsize=18)
     ds.despine(ax=ax)
+    mpla.setp(
+        obj=ax.get_xticklabels(),
+        rotation=45,
+        ha="right",
+        rotation_mode="anchor"
+    )
     fig.savefig(fname=PATH_GRAPH_SVG, format=FORMAT, bbox_inches="tight")
     print(df)
 
