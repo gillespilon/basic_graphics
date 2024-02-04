c7c562ccfda2eb23a608af709c1af6905a7b9e94
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-02-03 18:21:49 -0600

    Add code to add graph

--- argpar.py
+++ argpar.py
@@ -11,7 +11,6 @@ import datasense as ds
 
 def main():
     parser = argparse.ArgumentParser(description="Create a waterfall chart")
-
     parser.add_argument(
         "-pf",
         "--path_or_file",
@@ -87,6 +86,7 @@ def main():
         "-gf",
         "--graph_format",
         default="svg",
+        choices=["svg", "png", "jpg"],
         type=str,
         required=False,
         help="Provide a string of the graph format (optional)",
@@ -99,28 +99,44 @@ def main():
         required=False,
         help="Provide a string of the graph title (optional)",
     )
-
     args = parser.parse_args()
-
     HEADER_TITLE = "Waterfall Report"
     HEADER_ID = "waterfall-report"
     OUTPUT_URL = "waterfall.html"
     original_stdout = ds.html_begin(
         output_url=OUTPUT_URL, header_title=HEADER_TITLE, header_id=HEADER_ID
     )
-    # add code here
-    print("Path or file       :", args.path_or_file)
+    df = ds.read_file(file_name=args.path_or_file)
+    df = ds.waterfall(
+        df=df,
+        path_in=args.path_or_file,
+        last_column=args.last_column,
+        ylim_min=args.y_axis_min,
+        ylim_max=args.y_axis_max,
+        positive_colour=args.positive_colour,
+        negative_colour=args.negative_colour,
+        first_bar_colour=args.first_bar_colour,
+        last_bar_colour=args.last_bar_colour,
+        grid_alpha=args.grid_alpha,
+        graph_format=args.graph_format,
+        title=args.graph_title,
+    )
+    print("data path or file  :", args.path_or_file)
     print("Last column        :", args.last_column)
     print("y axis min         :", args.y_axis_min)
     print("y axis max         :", args.y_axis_max)
     print("positive colour    :", args.positive_colour)
     print("negative colour    :", args.negative_colour)
     print("first bar colour   :", args.first_bar_colour)
-    print("last   bar colour  :", args.last_bar_colour)
+    print("last bar colour    :", args.last_bar_colour)
     print("grid alpha         :", args.grid_alpha)
     print("graph format       :", args.graph_format)
     print("graph title        :", args.graph_title)
-
+    path_graph = args.path_or_file.with_suffix("." + args.graph_format)
+    print("graph path or file :", path_graph)
+    print()
+    print(df)
+    print()
     ds.script_summary(script_path=Path(__file__), action="finished at")
     ds.html_end(original_stdout=original_stdout, output_url=OUTPUT_URL)
 
